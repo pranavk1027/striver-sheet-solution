84a697cbca100a7ab1d60a9f664f01dfb518cbe7
pranavky12 <125976312+pranavky12@users.noreply.github.com>
2024-07-26 13:13:07 +0530

    Create Largest Element in Array.cpp

---
+++ Largest Element in Array.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        int max=INT_MIN;
+        for(int i=0;i<n;i++){
+           if(max<arr[i])max=arr[i];
+        }
+        return max;
+    }
+};
